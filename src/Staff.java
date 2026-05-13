e343d379a070a78b38b5e2881973275abc2428f2
Ratul Kumar Datta Aumit <2024100000480@seu.edu.bd>
2026-05-13 21:14:47 +0600

    Update Staff.java

--- src/Staff.java
+++ src/Staff.java
@@ -1,2 +1,13 @@
-public class Staff {
+public class Staff extends User {
+    public Staff(int userId, String userName, String password) {
+        super(userId, userName, password, "Staff");
+    }
+
+    @Override
+    public void login() {
+        System.out.println("Staff login successful.");
+    }
+
+    public void recordPayment() { System.out.println("Payment recorded."); }
+    public void checkInMember() { System.out.println("Member check-in completed."); }
 }
