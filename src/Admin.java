c2e60c80d81c11ac9edb102168f51e00f00f9cc7
rifah-tasnia256 <2024100000256@seu.edu.bd>
2026-05-13 17:59:45 +0600

    Added logic for Admin and Trainer classes

--- src/Admin.java
+++ src/Admin.java
@@ -1,2 +1,17 @@
-public class Admin {
+import java.util.List;
+
+public class Admin extends User {
+    public Admin(int userId, String userName, String password) {
+        super(userId, userName, password, "Admin");
+    }
+
+    @Override
+    public void login() {
+        System.out.println("Welcome : " + getUserName() + ". Access granted.");
+    }
+
+    public void addMember() { System.out.println("Member creation initiated."); }
+    public void deleteMember() { System.out.println("Member record deleted."); }
+    public void addTrainer() { System.out.println("Trainer record added."); }
+    public void createPlan() { System.out.println("New membership plan created."); }
 }
--- src/Trainer.java
+++ src/Trainer.java
@@ -1,2 +1,23 @@
 public class Trainer {
+    private int trainerId;
+    private String name, specialty, phone;
+
+    public Trainer(int trainerId, String name, String specialty, String phone) {
+        this.trainerId = trainerId;
+        this.name = name;
+        this.specialty = specialty;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getTrainerId() {
+        return trainerId;
+    }
+    public void showTrainer() {
+        System.out.println("\n--- Trainer Profile ---");
+        System.out.println("ID: " + trainerId + " \nName: " + name + " \nSpecialty: " + specialty);
+        System.out.println("----------------------");
+    }
 }
