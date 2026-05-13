968e7f7af09327a161653e872600eec309b2c952
mdsiamhasansaikot92 <mdsiamhasansaikot92@proton.me>
2026-05-13 22:43:44 +0600

    Added membership and payment classes

--- src/MemberShip.java
+++ src/MemberShip.java
@@ -1,2 +1,30 @@
+import java.time.LocalDate;
+
 public class MemberShip {
+    public int planId;
+    public String planName;
+    public int duration;
+    public double fee;
+    public String expiryDate;
+
+    public MemberShip(int planId, String planName, int duration, double fee, String expiryDate) {
+        this.planId = planId;
+        this.planName = planName;
+        this.duration = duration;
+        this.fee = fee;
+        this.expiryDate = expiryDate;
+        this.expiryDate = LocalDate.now().plusMonths(duration).toString();
+    }
+
+    public void createPlan() {
+        System.out.println("Plan '" + planName + "' created.");
+    }
+
+    public void checkExpiry() {
+        System.out.println("Expiry Date: " + expiryDate);
+    }
+
+    public void renewMembership() {
+        System.out.println("Membership renewed for " + planName);
+    }
 }
--- src/Payment.java
+++ src/Payment.java
@@ -1,2 +1,28 @@
 public class Payment {
+    private int paymentId, memberId;
+    private double amount;
+    private String paymentDate;
+    private String memberName;
+
+    public Payment(int paymentId, int memberId, double amount, String paymentDate) {
+        this.paymentId = paymentId;
+        this.memberId = memberId;
+        this.amount = amount;
+        this.paymentDate = paymentDate;
+        this.memberName = memberName;
+    }
+
+    public void recordPayment() {
+        System.out.println("Payment of " + amount + " recorded for " + memberId + ".");
+    }
+
+    public void showPaymentHistory() {
+        System.out.println("Member Name : " + memberName + "\nMember Id : " + memberId +
+                "\nDate: " + paymentDate + " \nAmount: " + amount);
+    }
+
+    public void generateReceipt() {
+        System.out.println("--- Receipt ---" + "\n ID: " + memberId +
+                "\nAmount: " + amount + "\n----------------");
+    }
 }
