980938f35f26edda803172a7186c80c4171e132c
tasniazeba <tasniazeba69@gmail.com>
2026-05-13 22:51:43 +0600

    Add BMI calculation and category determination methods

--- src/BMI.java
+++ src/BMI.java
@@ -1,2 +1,9 @@
 public class BMI {
+    public static double calculateBMI(double h, double w) { return w / (h * h); }
+    public static String getBMICategory(double bmi) {
+        if (bmi < 18.5) return "Underweight";
+        else if (bmi < 25) return "Normal";
+        else if (bmi < 30) return "Overweight";
+        else return "Obese";
+    }
 }
