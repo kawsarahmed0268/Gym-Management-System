30d94f8b18fdf53dc2e503de669d972f8776cd07
tasniazeba <tasniazeba69@gmail.com>
2026-06-02 19:53:59 +0600

    Refactor Attendance constructor to set date and time

--- src/Attendance.java
+++ src/Attendance.java
@@ -1,18 +1,31 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Attendance {
     private int attendanceId, memberId;
-    private String date, checkInTime;
+    private String date;
+    private String checkInTime;
 
-    public Attendance(int attendanceId, int memberId, String date, String checkInTime) {
+    public Attendance(int attendanceId, int memberId) {
         this.attendanceId = attendanceId;
         this.memberId = memberId;
-        this.date = date;
-        this.checkInTime = checkInTime;
+        this.date = LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        this.checkInTime = LocalTime.now().format(DateTimeFormatter.ofPattern("hh:mm a"));
     }
 
     public void checkIn() {
-        System.out.println("Check-in successful on " + date);
+        System.out.println("Check-in successful!");
+        System.out.println("Member ID   : " + memberId);
+        System.out.println("Date        : " + date);
+        System.out.println("Time        : " + checkInTime);
     }
+
     public void showAttendanceHistory() {
-        System.out.println("Date: " + date + " | Time: " + checkInTime);
+        System.out.println("Member ID : " + memberId + " | Date: " + date + " | Time: " + checkInTime);
     }
+
+    public int getMemberId()     { return memberId; }
+    public String getDate()      { return date; }
+    public String getCheckInTime() { return checkInTime; }
 }
