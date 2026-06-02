deebfb4eab572da279286880c2a426b42a40f85a
tasniazeba <tasniazeba69@gmail.com>
2026-06-02 19:54:36 +0600

    Update BMI.java

--- src/BMI.java
+++ src/BMI.java
@@ -1,5 +1,8 @@
 public class BMI {
-    public static double calculateBMI(double h, double w) { return w / (h * h); }
+    public static double calculateBMI(double h, double w) {
+        return w / (h * h);
+    }
+
     public static String getBMICategory(double bmi) {
         if (bmi < 18.5) return "Underweight";
         else if (bmi < 25) return "Normal";
